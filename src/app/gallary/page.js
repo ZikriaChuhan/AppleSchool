16aa8f63a9293c8092e77968fb41cd31654d464b
ZikriaChuhan <154091659+ZikriaChuhan@users.noreply.github.com>
2024-11-01 23:28:10 +0500

    Create gallary page.js

---
+++ src/app/gallary/page.js
@@ -0,0 +1,43 @@
+import "./gallaryStyle.css"
+import "../style.css";
+import "../aboutus/aboutStyle.css";
+
+
+import Image from "next/image";
+import CloudImg from "../image/cloudimg.png"
+import FooterCloudImg from "../image/Footer-cloud-img.png"
+
+import Header from "../components/header";
+import Footer from "../components/footer";
+import BannerSectionGallary from "../gallaryComponent/gallaryBannnerSection";
+import SectionTwoGallary from "../gallaryComponent/sectionTwo";
+
+
+
+
+export default function Blog() {
+    return (
+        <>
+
+            <Header />
+
+            <section>
+                <Image src={CloudImg} className="cloudImgabout" alt="Cloud Img" />
+            </section>
+
+            <BannerSectionGallary />
+
+            <SectionTwoGallary />
+
+           
+
+            <div className="FooterCloudImgDiv">
+                <Image src={FooterCloudImg} alt="FooterCloudImg" className="FooterCloudImg" />
+            </div>
+
+            <Footer />
+
+
+        </>
+    );
+}
