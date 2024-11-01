d0970a62717865f3b9bbc31563ce0c526d1bff71
ZikriaChuhan <154091659+ZikriaChuhan@users.noreply.github.com>
2024-11-01 23:31:23 +0500

    Create gallaryBannerSection.js

---
+++ src/app/gallaryComponent/gallaryBannerSection.js
@@ -0,0 +1,19 @@
+
+export default function BannerSectionGallary() {
+    return (
+        <>
+
+            <section className="aboutfirstbanner flex flex-col items-center justify-center ">
+                <h2>
+                    Gallary
+                </h2>
+                <div className="flex items-center justify-center gap-3">
+                    <h4>Home</h4>
+                    <span>/</span>
+                    <h3>Gallary</h3>
+                </div>
+            </section>
+
+        </>
+    );
+}
